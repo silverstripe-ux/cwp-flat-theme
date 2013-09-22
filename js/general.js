d7e3ed8e92a30797634ff95de93737598cf05b6a
Jeremy Bridson <jeremy@silverstripe.com>
2013-09-23 10:25:47 +1200

    ENHANCEMENT: Homepage responsive styling - carousel js fixes for small screen sizes.

--- js/general.js
+++ js/general.js
@@ -7,7 +7,19 @@ $(function() {
 	// Hello there! You can add in any of your site-specific code here.
 
 	//Set height of carousel caption to 100% of container
-	var carouselHeight = $('.carousel-inner').outerHeight();
-	$('.carousel-caption').height(carouselHeight);
+	function CaptionHeight() {
+		$('.carousel-caption').removeAttr('style');
+		if($(window).width() > 1100){
+			var carouselHeight = $('.carousel-inner').outerHeight();
+			$('.carousel-caption').each(function(){
+				$(this).height(carouselHeight);
+			});
+		}
+	}
+
+	CaptionHeight();
+	$(window).resize(function(){
+		CaptionHeight();
+	});
 });
 
