6710d0a26cfc2efb5e47aa2d2dcbd2376178badf
Jeremy Bridson <jeremy@silverstripe.com>
2013-09-23 09:25:32 +1200

    ENHANCEMENT: added some js to set the height of the carousel caption to match the height of the image

--- js/general.js
+++ js/general.js
@@ -6,5 +6,8 @@ $(function() {
 
 	// Hello there! You can add in any of your site-specific code here.
 
+	//Set height of carousel caption to 100% of container
+	var carouselHeight = $('.carousel-inner').outerHeight();
+	$('.carousel-caption').height(carouselHeight);
 });
 
